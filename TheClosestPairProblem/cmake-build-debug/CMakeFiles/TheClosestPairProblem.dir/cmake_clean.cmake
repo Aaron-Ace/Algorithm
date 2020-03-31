296ada3c8d3a86483478cc9aee2490186e54b744
Aaronace <andrewzhang0910@gmail.com>
2020-03-31 13:22:15 +0800

    Finish-

---
+++ TheClosestPairProblem/cmake-build-debug/CMakeFiles/TheClosestPairProblem.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TheClosestPairProblem.dir/main.cpp.obj"
+  "TheClosestPairProblem.exe"
+  "TheClosestPairProblem.exe.manifest"
+  "TheClosestPairProblem.pdb"
+  "libTheClosestPairProblem.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TheClosestPairProblem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
