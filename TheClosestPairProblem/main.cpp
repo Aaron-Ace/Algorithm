563c86b16850499d812cdee6bb962b319c139ff0
Aaronace <andrewzhang0910@gmail.com>
2020-04-01 10:21:56 +0800

    Finish-->pass the judge

--- TheClosestPairProblem/main.cpp
+++ TheClosestPairProblem/main.cpp
@@ -30,10 +30,10 @@ double mindist( int a, int b )
 {
     if ( a > b ) {return 40004;}//切到只剩1個點，return int
     int l = (a+b)/2,r = (a+b)/2,mid = (a+b)/2; //line:左集合與右集合的中間線x座標
-    double d = min( mindist( a, mid-1 ), mindist( mid+1, b ) );
+    double d = min( mindist( a, mid-1 ), mindist( mid+1, b ) );//一直左右分找到最小的距離
 
-    while ( l >= a && d > P[mid].x-P[l].x ) l --;
-    while ( r <= b && d > P[r].x-P[mid].x ) r ++;
+    while ( l >= a && d > P[mid].x-P[l].x ){ l --;}
+    while ( r <= b && d > P[r].x-P[mid].x ){ r ++;}
 
     for ( int i = l+1 ; i < r ; ++ i )
         for ( int j = i+1 ; j < r ; ++ j )
@@ -50,7 +50,7 @@ int main()
     {
         if(n==0){break;}
         for ( int i = 0 ; i < n ; ++ i )
-            cin>>P[i].x>>P[i].y;
+		{cin>>P[i].x>>P[i].y;}
 
         sort( P, P+n, cmp );
         double d = mindist( 0, n-1 );
